e9bd09e93825e67c31656a305a761d5d7c904e74
suwigyarathore <Suwi.Rathore@klm.com>
2018-03-09 21:51:19 +0100

    creating first observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable'
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'app';
+  observable$: Observable<any>;
+  subscription: Subscription;
+
+  ngOnInit () {
+    this.observable$ = Observable.create((observer) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.complete();
+    });
+
+    this.subscription = this.observable$.subscribe(
+      value => console.log(value),
+      err => { },
+      () => console.log('this is the end')
+    );
+  }
+
+  ngOnDestroy () {
+    this.subscription.unsubscribe();
+  }
 }
