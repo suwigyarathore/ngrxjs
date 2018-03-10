942bd309d3c713a8ea0aa40e95320a0a08e63901
suwigyarathore <Suwi.Rathore@klm.com>
2018-03-10 01:33:40 +0100

    adding operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable'
 import { Subscription } from 'rxjs/Subscription';
+import { Subject } from 'rxjs/Subject';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +19,11 @@ import { Subscription } from 'rxjs/Subscription';
 export class AppComponent implements OnInit, OnDestroy {
   title = 'app';
   observable$: Observable<any>;
+  subject$: Subject<any>;
+  replaySubject$: ReplaySubject<any>;
   subscription: Subscription;
+  subjectSubscription: Subscription;
+  replaySubjectSubscription: Subscription;
 
   ngOnInit () {
     this.observable$ = Observable.create((observer) => {
@@ -25,9 +38,64 @@ export class AppComponent implements OnInit, OnDestroy {
       err => { },
       () => console.log('this is the end')
     );
+
+    this.subject$ = new Subject();
+    this.subjectSubscription = this.subject$.subscribe(x => console.log('Subject Subscription', x));
+    this.subject$.next(1);
+    this.subject$.next(4);
+
+
+    this.replaySubject$ = new ReplaySubject();
+    this.replaySubjectSubscription = this.replaySubject$.subscribe(x => console.log('Replay Subject Subscription', x));
+    this.replaySubject$.next(1);
+    this.replaySubject$.next(4);
+    this.replaySubject$.subscribe(x => console.log('Replay Subject Second Subscription', x));
+    this.replaySubject$.next(20);
+
+    //this.takeOperator();
+    //this.mapOperator();
+    //this.filterOperator();
+    //this.mergeOperator();
+    this.switchMapOperator();
+  }
+
+  takeOperator () {
+    const numbers$ = Observable.interval(1000).take(5);
+    numbers$.subscribe(x => console.log(x));
+  }
+
+  mapOperator () {
+    const numbers$ = Observable.interval(1000);
+    numbers$
+      .take(5)
+      .map(x => x * 10)
+      .subscribe(x => console.log(x));
+  }
+
+  filterOperator () {
+    const numbers$ = Observable.interval(1000);
+    numbers$
+      .take(5)
+      .filter(x => x % 2 === 0)
+      .subscribe(x => console.log(x));
+  }
+
+  mergeOperator () {
+    const numbers$ = Observable.interval(1000);
+    const letters$ = Observable.of('a', 'b', 'c', 'd', 'e');
+    letters$.mergeMap(x => numbers$.take(5).map(i => i + x))
+      .subscribe(x => console.log(x));
+  }
+
+  switchMapOperator () {
+    const numbers$ = Observable.interval(1000);
+    const letters$ = Observable.of('a', 'b', 'c', 'd', 'e');
+    letters$.switchMap(x => numbers$.take(5).map(i => i + x))
+      .subscribe(x => console.log(x));
   }
 
   ngOnDestroy () {
     this.subscription.unsubscribe();
+    this.subjectSubscription.unsubscribe();
   }
 }
